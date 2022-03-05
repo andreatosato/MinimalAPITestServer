0a2828734122d34fbc1787f12725c6c1156a70cc
Andrea Tosato <andrea.tosato@hotmail.it>
2022-03-05 21:02:38 +0100

    Startup

---
+++ src/MinimalApiServer.Tests/MinimalApiApplicationFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+
+namespace MinimalApiServer.Tests;
+
+public class MinimalApiApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        var host = base.CreateHost(builder);
+        return host;
+    }
+}
